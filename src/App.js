87a0aadcf382252f71d4a76af24440a399631c0d
HUN-sp <chopravinaykumarchopra@gmail.com>
2024-05-29 13:05:00 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar.js';
+import TypeWriter from './TypeWriter.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar/>
+        <TypeWriter
+         text ={[
+          "Hello, I m vinay",
+          "I m A software developer",
+          "okay good bye"
+         ]}
+         typingSpeed={100}
+         deletingSpeed={50}
+         duration={1000}
+        />
       </header>
     </div>
   );
