552255627b090c0b96a2f891e71fec0d084f388f
HUN-sp <chopravinaykumarchopra@gmail.com>
2024-05-29 13:23:27 +0530

    Displayed my name

--- src/App.js
+++ src/App.js
@@ -6,17 +6,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <NavBar/>
-        <TypeWriter
-         text ={[
-          "Hello, I m vinay",
-          "I m A software developer",
-          "okay good bye"
-         ]}
-         typingSpeed={100}
-         deletingSpeed={50}
-         duration={1000}
-        />
+        <NavBar />
+        <div className='typewriter-container'>
+          <div className="fixed-name">
+            <h3>Vinay Kumar Chopra</h3>
+          </div>
+          <TypeWriter
+            text={[
+              "Hello, I m vinay",
+              "I m A software developer",
+              "okay good bye"
+            ]}
+            typingSpeed={100}
+            deletingSpeed={50}
+            duration={1000}
+          />
+        </div>
       </header>
     </div>
   );
